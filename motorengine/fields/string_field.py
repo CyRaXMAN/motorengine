bb9c08090dee302311cd82feb2824629f1c2b0d7
CyRaXMAN <cyraxman0s@gmail.com>
2015-03-20 15:55:07 +0600

    String field additional validations

--- motorengine/fields/string_field.py
+++ motorengine/fields/string_field.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import re
 import six
+import collections
 
 from motorengine.fields.base_field import BaseField
 
@@ -14,29 +16,51 @@ class StringField(BaseField):
 
     .. testcode:: modeling_fields
 
-        name = StringField(required=True, max_length=255)
+        name = StringField(required=True, max_length=255, min_length=1)
 
     Available arguments (apart from those in `BaseField`):
 
-    * `max_length` - Raises a validation error if the string being stored exceeds the number of characters specified by this parameter
+    * `max_length` - Raises a validation error if the string has amount of characters more than specified
+    * `min_length` - Raises a validation error if the string has amount of characters less than specified
+    * `choices` - Raises a validation error if the string not in specified choices
+    * `regex` - Raises a validation error if the string does not match specified regex
     '''
 
-    def __init__(self, max_length=None, *args, **kw):
+    def __init__(self, max_length=None, min_length=None, choices=None, regex=None, *args, **kw):
         super(StringField, self).__init__(*args, **kw)
         self.max_length = max_length
+        self.min_length = min_length
+        self.choices = choices
+        self.regex = regex
 
     def validate(self, value):
         if value is None:
             return True
 
-        is_string = isinstance(value, six.string_types)
-
-        if not is_string:
+        if not isinstance(value, six.string_types):
             return False
 
-        below_max_length = self.max_length is not None and len(value) <= self.max_length
+        if self.max_length is not None and len(value) > self.max_length:
+            return False
 
-        return self.max_length is None or below_max_length
+        if self.min_length is not None and len(value) < self.min_length:
+            return False
+
+        if self.regex is not None:
+            try:
+                regex = re.compile(self.regex)
+                if not regex.match(value):
+                    return False
+            except re.error as err:
+                raise ValueError("Invalid regex: %s" % err)
+
+        if self.choices is not None:
+            if not isinstance(self.choices, collections.Iterable):
+                raise ValueError("'choices' must be an iterable")
+            if value not in self.choices:
+                return False
+
+        return True
 
     def is_empty(self, value):
         return value is None or value == ""
--- tests/fields/test_string_field.py
+++ tests/fields/test_string_field.py
@@ -10,6 +10,7 @@ from tests import AsyncTestCase
 class TestStringField(AsyncTestCase):
     def test_create_string_field(self):
         field = StringField(db_field="test", max_length=200)
+
         expect(field.db_field).to_equal("test")
         expect(field.max_length).to_equal(200)
 
@@ -24,8 +25,15 @@ class TestStringField(AsyncTestCase):
         expect(field.validate("-----")).to_be_true()
         expect(field.validate("-----" * 2)).to_be_false()
 
+    def test_validate_enforces_minlength(self):
+        field = StringField(min_length=5)
+
+        expect(field.validate("------")).to_be_true()
+        expect(field.validate("----")).to_be_false()
+
     def test_is_empty(self):
         field = StringField()
+
         expect(field.is_empty(None)).to_be_true()
         expect(field.is_empty("")).to_be_true()
         expect(field.is_empty("123")).to_be_false()
@@ -34,3 +42,37 @@ class TestStringField(AsyncTestCase):
         field = StringField(required=False)
 
         expect(field.validate(None)).to_be_true()
+
+    def test_choice(self):
+        field = StringField(choices=["one", "two", "three"])
+
+        expect(field.validate("one")).to_be_true()
+        expect(field.validate("four")).to_be_false()
+
+    def test_invalid_choice_type(self):
+        try:
+            StringField(choices=1).validate("a")
+        except ValueError as err:
+            expect(err).to_have_an_error_message_of("'choices' must be an iterable")
+        else:
+            expect.not_to_be_here()
+
+    def test_empty(self):
+        field = StringField(required=True)
+
+        expect(field.validate(None)).to_be_true()
+
+    def test_valid_regex(self):
+        field = StringField(regex="[a-z0-9]+")
+
+        expect(field.validate("test1")).to_be_true()
+        expect(field.validate("Test1")).to_be_false()
+
+    def test_invalid_regex(self):
+        try:
+            StringField(regex='][').validate('a')
+        except ValueError as err:
+            expect(err).to_have_an_error_message_of("Invalid regex: unexpected end of regular expression")
+        else:
+            expect.not_to_be_here()
+
